f759e92998d7b64b88aedb6853be75fc0a5fb49f
Luiz Gilberto <luizgilberto@gmail.com>
2021-07-02 13:49:10 +1000

    created the getJSON for the videos/trailers

--- public/js/jquery.js
+++ public/js/jquery.js
@@ -23,4 +23,21 @@ $.getJSON(`${tmdb_base_url}/discover/movie${api_query}`, function(data) {
     const movieEntry = $("<div>").append(`<img src="${image_url}${posterImage}">`)
     $("#movies").append(movieEntry)
   })
-})
+});
+
+
+// show the movie video (from youtube or vimeo) at class ".movie-video"   . Need to define the "movie_id"?
+$.getJSON(`${tmdb_base_url}/movie/${movie_id}/videos${api_query}&language=en-US`,
+  function (data) {
+    let video_URL = "";
+    if (data.results[0].site === "YouTube") {
+      video_URL = `https://www.youtube.com/embed/${data.results[0].key}`;
+    } else if (data.results[0].site === "Vimeo") {
+      video_URL = `https://vimeo.com/${data.results[0].key}`;
+    }
+    $(".movie-video").attr("src", video_URL);
+  })
+  .catch((err) => {
+    return res.send(err.message)
+  });
+
